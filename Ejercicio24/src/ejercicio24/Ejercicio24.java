fef6519b87e2c28dc7c057334d6480339eb0c102
arturo <arturo@ArturoMSI>
2018-04-11 14:31:47 +0200

    Ejercicio finalizado

---
+++ Ejercicio24/src/ejercicio24/Ejercicio24.java
@@ -0,0 +1,16 @@
+package ejercicio24;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Ejercicio24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaCirculos ventana=new VentanaCirculos();
+    }
+
+}
